caf53c19ed52765d07d1690f545ce0a4b2f38e35
NachoGalvagno <ignacio.101@hotmail.com>
2022-05-01 19:58:19 -0300

    first commit

--- src/components/Countdown/styles.js
+++ src/components/Countdown/styles.js
@@ -1,6 +1,52 @@
 import styled from 'styled-components';
 
 export const Time = styled.p`
+  color: white;
+  font-size: 100px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+`;
+
+export const Text = styled.p`
   color: red;
-  font-size: 32px;
+  font-size: 40px;
+  text-align: center; 
+  margin: 0 25px;
+  padding: 0;
+  position: relative; 
+  left: 18px
+`;
+
+export const MiniContainer = styled.div`
+  position: relative; 
+  right: 9px
+`;
+
+export const Background = styled.div`
+  background-color: black;
+  height: 100%
+`;
+
+export const SmallContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Container = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const BigContainer = styled.div`
+  background-color: red;
+  height: 600px;
+  display: flex;
+  align-items: center;
+
 `;
--- src/components/Countup/index.js
+++ src/components/Countup/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Time } from './styles';
+import { Time, Container, BigContainer, Text, SmallContainer, MiniContainer, Background} from './styles';
 
 const Countup = ({}) => {
-  return <Time>00:00:00</Time>;
+  return(
+    <BigContainer>
+      <Container>
+        <Background>
+          <Time>00 : 00 : 00</Time>
+          <SmallContainer>
+            <MiniContainer>
+              <Text>Hours</Text>
+            </MiniContainer>
+            <Text>Minutes</Text>
+            <Text>Seconds</Text>
+          </SmallContainer>
+        </Background>
+      </Container>
+    </BigContainer>
+  ) 
 };
 
 Countup.propTypes = {
   time: PropTypes.arrayOf(PropTypes.number),
 };
-Countup.defaultProps = {
+Countup.defaultProps = { 
   time: [],
 };
 
